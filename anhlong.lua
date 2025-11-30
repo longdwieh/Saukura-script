19a3ebbe1fceb74e40c448308f279e592bebea1d
longdwieh <longhyaz2012@gmail.com>
2025-11-30 15:22:55 +0700

    Add files via upload

---
+++ anhlong.lua
@@ -0,0 +1,6 @@
+local Flux = loadstring(game:HttpGet("https://raw.githubusercontent.com/8e8f8f8f8f/UiFlux/main/Source.lua"))()
+
+local win = Flux:Window("Long Hub", "Version 1.0", Color3.fromRGB(100, 50, 255), Enum.KeyCode.RightControl)
+
+local tab = win:Tab("Farming", "rbxassetid://4483345998")
+local tab = win:Tab("Longdeptrai")
